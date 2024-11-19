946871e6eb9a55b1c0cc54e71922ae393b6f5e1e
Kallepally Srinivas <kallepallisrinivas1@gmail.com>
2024-11-19 14:57:22 +0530

    addition of two numbers

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition
+{
+public int add(int a,int b)
+{
+int c=a+b;
+return c;
+}
+public static void main(String args[])
+{
+Addition add = new Addition();
+System.out.println("The sum of two numbers is "+(cal.add(2,3)));
+}
+}
