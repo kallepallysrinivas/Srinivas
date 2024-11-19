8d5161159ac024eedf0e6c4c541ccbb28b7fa7e0
Kallepally Srinivas <kallepallisrinivas1@gmail.com>
2024-11-19 15:43:45 +0530

    I added the other information 5

--- Addition.java
+++ Addition.java
@@ -1,19 +1,6 @@
 public class Addition
 {
-public int add(int a,int b)
-{
-int c=a+b;
-return c;
-}
-public int square(int x)
-{
-	int z=x*x;
-	return z;
-}
-public static void main(String args[])
-{
-Addition cal = new Addition();
-System.out.println(cal.add(2,3));
-System.out.println(cal.square(41));
-}
+	public static void main(String args[] )
+	{
+		System.out.println("kallepally srinivas");}
 }
